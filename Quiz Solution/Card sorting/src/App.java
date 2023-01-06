8173a5395fed378db8f4a3be41b8fb744ae8cf87
KDEVCORE <kdevcore@gmail.com>
2023-01-06 13:59:26 +0900

    Quiz solution, Card sorting

--- Quiz Solution/Card sorting/src/App.java
+++ Quiz Solution/Card sorting/src/App.java
@@ -5,10 +5,10 @@ import java.util.PriorityQueue;
 public class App {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()); // 1 <= n <= 100000
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i=0; i<n; i++) {
-            int data = Integer.parseInt(br.readLine());
+            int data = Integer.parseInt(br.readLine()); // 1 <= data <= 1000
             pq.add(data);
         }
         int data1 = 0, data2 = 0, sum=0;
