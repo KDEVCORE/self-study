eafc13fde9264d6bf4d990c6a74f7628920460ac
KDEVCORE <kdevcore@gmail.com>
2023-01-09 11:08:46 +0900

    Quiz solution, Find the prime number

--- Quiz Solution/Find the prime number/src/App.java
+++ Quiz Solution/Find the prime number/src/App.java
@@ -8,18 +8,20 @@ public class App {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken()); // 1 <= start <= end <= 1000000
-        int[] a = new int[end+1];
+        boolean[] a = new boolean[end+1];
         for(int i=2; i<=end; i++) {
-            a[i] = i;
+            a[i] = true;
         }
         for(int i=2; i<Math.sqrt(end); i++) {
-            if(a[i] == 0) continue;
+            if(!a[i]) continue;
             for(int j=i+i; j<=end; j+=i) {
-                a[j] = 0;
+                a[j] = false;
             }
         }
+        StringBuilder sb = new StringBuilder();
         for(int i=start; i<=end; i++) {
-            if(a[i] != 0) System.out.println(a[i]);
+            if(a[i]) sb.append(i).append("\n");
         }
+        System.out.println(sb);
     }
 }
