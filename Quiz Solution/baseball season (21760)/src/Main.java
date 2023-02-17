e78337243b5234c3961797e06b497f5ba1de2384
KDEVCORE <kdevcore@gmail.com>
2023-02-17 10:50:08 +0900

    challenge retired, equation reference

--- Quiz Solution/baseball season (21760)/src/Main.java
+++ Quiz Solution/baseball season (21760)/src/Main.java
@@ -7,20 +7,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
         int leagues, teams, k, limit;
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < testCase; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             leagues = Integer.parseInt(st.nextToken());
             teams = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
             limit = Integer.parseInt(st.nextToken());
-            int result = 0;
-            while(result <= limit) {
-                int gameB = (teams - 1) * teams;
-                int gameA = k * gameB;
-                result = Math.max(result, gameA + gameB);
-                teams++;
+            long b = 0;
+            while(leagues*teams*b*(k*(teams-1)+teams*(leagues-1)) <= (2*limit)) b++;
+            if(b <= 1) {
+                sb.append(-1 + "\n");
+                continue;
             }
-            System.out.println(result);
+            long a = leagues * teams * (b-1) * (k*(teams-1)+teams*(leagues-1));
+            sb.append(a/2 + "\n");
         }
+        System.out.print(sb);
     }
 }
