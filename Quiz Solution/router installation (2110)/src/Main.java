34e3d80899ccee794b906c9fda2e86ab13709a56
KDEVCORE <kdevcore@gmail.com>
2023-02-22 11:16:50 +0900

    challenge success, got help

--- Quiz Solution/router installation (2110)/src/Main.java
+++ Quiz Solution/router installation (2110)/src/Main.java
@@ -9,10 +9,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int house = Integer.parseInt(st.nextToken());
         int router = Integer.parseInt(st.nextToken());
-        int[] location = new int[house + 1];
-        for(int i=1; i<=house; i++) location[i] = Integer.parseInt(br.readLine());
+        int[] location = new int[house];
+        for(int i=0; i<house; i++) location[i] = Integer.parseInt(br.readLine());
         Arrays.sort(location);
-
-        
+        int start = 0, end = location[house-1] - location[0], result = Integer.MIN_VALUE;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            int count = 1;
+            int frontHouse = location[0];
+            for(int i=1; i<house; i++) {
+                if(location[i] - frontHouse >= mid) {
+                    count++;
+                    frontHouse = location[i];
+                }
+            }
+            if (count >= router) {
+                result = Math.max(result, mid);
+                start = mid+1;
+            } else end = mid-1;
+        }
+        System.out.println(result);
     }
 }
