a612b8473e741c84b3ada1fcf22ed8db26f924bd
KDEVCORE <kdevcore@gmail.com>
2023-03-02 11:35:12 +0900

    challenge failed, time over

--- Search/beer festival (17503)/src/Main.java
+++ Search/beer festival (17503)/src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,29 +13,34 @@ public class Main {
         int target = Integer.parseInt(st.nextToken());
         int beers = Integer.parseInt(st.nextToken());
         Beer[] beerType = new Beer[beers];
-        ArrayList<Index> indexList = new ArrayList<>();
         int maxLevel = Integer.MIN_VALUE;
         for(int i=0; i<beers; i++) {
             st = new StringTokenizer(br.readLine());
             beerType[i] = new Beer(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
             maxLevel = Math.max(maxLevel, beerType[i].level);
         }
+        Arrays.sort(beerType);
 
         int min = 1, max = maxLevel, result = Integer.MAX_VALUE;
         while(min <= max) {
             int mid = (min + max) / 2;
-            int day = 0, k = 1;
             long sum = 0;
-            for(int i=0; i<beers; i+=k) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int i=0; i<beers; i++) {
                 if(beerType[i].level <= mid) {
-                    sum += beerType[i].preference;
-                    day++;
-                    if(day == period) break;
+                    temp.add(beerType[i].preference);
+                } else {
+                    break;
                 }
             }
-            if(sum < target) {
+            if(temp.size() < period) {
                 min = mid + 1;
-            } else {
+                continue;
+            }
+            Collections.sort(temp, Collections.reverseOrder());
+            for(int i=0; i<period; i++) sum += temp.get(i);
+            if(sum < target) min = mid + 1;
+            else {
                 result = Math.min(result, mid);
                 max = mid - 1;
             }
@@ -51,7 +58,6 @@ class Beer implements Comparable<Beer> {
 
     @Override
     public int compareTo(Beer obj) {
-        if(this.preference == obj.preference) return this.level - obj.level;
-        return this.preference - obj.preference;
+        return this.level == obj.level ? obj.preference - this.preference : this.level - obj.level;
     }
 }
