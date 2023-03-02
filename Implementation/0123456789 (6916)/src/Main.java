8a83d808bbfb73d410cdc6948b9695b370d5b0d5
KDEVCORE <kdevcore@gmail.com>
2023-03-02 18:33:10 +0900

    challenge success

--- Implementation/0123456789 (6916)/src/Main.java
+++ Implementation/0123456789 (6916)/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
-        switch(number) {
+        switch(number) { // line 1
             case 0:
             case 2:
             case 3:
@@ -16,28 +16,32 @@ public class Main {
             case 9:
             System.out.println(" * * *");
             break;
+            default:
+            System.out.println();
         }
-        for(int i=0; i<3; i++) {
+        for(int i=0; i<3; i++) { // line 2 ~ 4
             switch(number) {
                 case 0:
                 case 4:
-                case 7:
                 case 8:
                 case 9:
                 System.out.println("*     *");
+                break;
+                case 1:
+                case 2:
+                case 3:
+                case 7:
+                System.out.println("      *");
                 break;
                 case 5:
                 case 6:
                 System.out.println("*");
                 break;
-                case 1:
-                case 2:
-                case 3:
-                System.out.println("      *");
-                break;
+                default:
+                System.out.println();
             }
         }
-        switch(number) {
+        switch(number) { // line 5
             case 2:
             case 3:
             case 4:
@@ -45,20 +49,17 @@ public class Main {
             case 6:
             case 8:
             case 9:
-            System.out.println(" * * *");
+                System.out.println(" * * *");
             break;
             default:
-            System.out.println("");
+                System.out.println();
         }
-        for(int i=0; i<3; i++) {
+        for(int i=0; i<3; i++) { // line 6 ~ 8
             switch(number) {
                 case 0:
                 case 6:
                 case 8:
                 System.out.println("*     *");
-                break;
-                case 2:
-                System.out.println("*");
                 break;
                 case 1:
                 case 3:
@@ -68,9 +69,14 @@ public class Main {
                 case 9:
                 System.out.println("      *");
                 break;
+                case 2:
+                System.out.println("*");
+                break;
+                default:
+                System.out.println();
             }
         }
-        switch(number) {
+        switch(number) { // line 9
             case 0:
             case 2:
             case 3:
@@ -78,7 +84,7 @@ public class Main {
             case 6:
             case 8:
             case 9:
-            System.out.println(" * * *");
+                System.out.println(" * * *");
             break;
         }
     }
