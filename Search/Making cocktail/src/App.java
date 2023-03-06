45be8b85a1a07683d0518337f14333f69a825cbb
KDEVCORE <kdevcore@gmail.com>
2023-03-06 19:35:52 +0900

    change directory

--- Search/Making cocktail/src/App.java
+++ Search/Making cocktail/src/App.java
@@ -48,5 +48,22 @@ public class App {
             }
         }
     }
-
+}
+class CNode {
+    int b, p, q;
+    public CNode(int b, int p, int q) {
+        super();
+        this.b = b;
+        this.p = p;
+        this.q = q;
+    }
+    public int getB() {
+        return b;
+    }
+    public int getP() {
+        return p;
+    }
+    public int getQ() {
+        return q;
+    }
 }
